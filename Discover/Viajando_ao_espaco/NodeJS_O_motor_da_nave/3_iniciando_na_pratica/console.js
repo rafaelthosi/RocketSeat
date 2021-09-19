708dea6523a07f3d2dd42cbf0c66c88963d18d42
rafaelthosi <thhrafael@outlook.com>
2021-09-19 20:26:17 -0300

    3 modulo finalizado

---
+++ Discover/Viajando_ao_espaco/NodeJS_O_motor_da_nave/3_iniciando_na_pratica/console.js
@@ -0,0 +1,10 @@
+console.log('Alguma coisa aqui');
+// terminal: Node console.js (ou Node console)""
+
+// console faz parte das variaveis globais, logo:
+global.console.log('Alguma coisa aqui, com global')
+
+// O que o global contém:
+console.log(global)
+
+// Mais sobre as variaveis globais: https://nodejs.org/api/globals.html
