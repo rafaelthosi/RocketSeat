9fa868e6c42b76df83f2cdbe90bc10dc22b917d0
rafaelthosi <thhrafael@outlook.com>
2021-11-07 05:20:01 -0300

    modulo FinAPI finalizado

--- Ignite/Node/Chapter-I/Finapi/src/index.js
+++ Ignite/Node/Chapter-I/Finapi/src/index.js
@@ -7,12 +7,33 @@ app.use(express.json())
 
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement - []
- */
+// Middleware
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  };
+
+  request.customer = customer;
+
+  next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    };
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -34,5 +55,94 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 
 });
+
+// app.use(verifyIfExistAccountCPF); // outro método para usar middlewares
+
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+})
+
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  };
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+})
+
+app.get("/statement/date", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00"); // fazer busca independente da hora
+
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return response.json(statement);
+});
+
+app.put("/account", verifyIfExistAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+})
+
+app.get("/account", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
+app.delete("/account", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+  
+  // splice
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers)
+})
+
+app.get("/balance", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+})
 
 app.listen(3333);
