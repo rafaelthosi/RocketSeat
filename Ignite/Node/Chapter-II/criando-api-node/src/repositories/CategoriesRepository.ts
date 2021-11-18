e1cbda0e68a562e1d305c447803fb7447f1d570b
rafaelthosi <thhrafael@outlook.com>
2021-11-18 01:22:12 -0300

    criando repository e usando DTO

---
+++ Ignite/Node/Chapter-II/criando-api-node/src/repositories/CategoriesRepository.ts
@@ -0,0 +1,35 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    /**
+     * Código abaixo é uma forma melhor e mais organizada de fazer:
+     * category.name = name
+     * category.description = description
+     * category.created_at = new Date()
+     */
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepository };
