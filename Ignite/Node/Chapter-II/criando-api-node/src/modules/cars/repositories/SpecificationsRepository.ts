33a96e4e7a7775d30eb490f177879fa839be2e75
rafaelthosi <thhrafael@outlook.com>
2021-12-02 21:36:35 -0300

    post specifications

---
+++ Ignite/Node/Chapter-II/criando-api-node/src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,34 @@
+import { Specification } from "../model/Specification";
+import {
+  ICreateSpecificationDTO,
+  ISpeceficationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpeceficationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
