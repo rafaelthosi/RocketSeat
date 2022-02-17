0e3e52f801cee641ecd2936c2c79657e869852b1
rafaelthosi <thhrafael@outlook.com>
2022-02-17 16:57:56 -0300

    youtube-masterclass-07typescript aula completa

--- Ignite/Node/Chapter-III/rentx/src/server.ts
+++ Ignite/Node/Chapter-III/rentx/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import "./database"
+
+import "./database";
+
+import "./shared/container";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-
 
 const app = express();
 
