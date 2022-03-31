f7d2985ff70f0f347f9585a233989e039af548c3
rafaelthosi <thhrafael@outlook.com>
2022-03-30 22:31:35 -0300

    criando middlewares de admin

--- ignite/node/chapter-IV/rentx/src/shared/infra/http/server.ts
+++ ignite/node/chapter-IV/rentx/src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import "reflect-metadata";
 
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
