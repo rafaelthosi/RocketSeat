5065a3ba135be669caa6a5c7df3d7ef5923f4230
rafaelthosi <thhrafael@outlook.com>
2022-07-11 17:41:25 -0300

    webpack dev server

--- ignite/react/01-github-explorer/webpack.config.js
+++ ignite/react/01-github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -10,6 +11,14 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  devServer: {
+    static: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
