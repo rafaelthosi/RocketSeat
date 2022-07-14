68178307ba56a78122e249e607664664cacdde51
rafaelthosi <thhrafael@outlook.com>
2022-07-14 01:02:24 -0300

    listando repositorios da rocketseat

--- ignite/react/01-github-explorer/src/components/RepositoryList.jsx
+++ ignite/react/01-github-explorer/src/components/RepositoryList.jsx
@@ -3,14 +3,6 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
-// https://api.github.com/orgs/rocketseat/repos
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
 
@@ -19,16 +11,15 @@ export function RepositoryList() {
       .then(response => response.json())
       .then(data => setRepositories(data))
   }, [])
-  
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
